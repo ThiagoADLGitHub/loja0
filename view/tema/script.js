ab351578af2d98a17dbfb7f609dd605046150808
ThiagoADLGitHub <126714187+ThiagoADLGitHub@users.noreply.github.com>
2023-04-04 12:24:21 -0300

    Commit html, css, reponsino esta ok, é só programar.

--- view/tema/script.js
+++ view/tema/script.js
@@ -10,11 +10,3 @@ menuIcon.addEventListener('click', () => {
 
 
 
-const menuCategoria = document.querySelector('#menu-categoria');
-const navCategoria = document.querySelector('.nav-categoria');
-
-menuIcon.addEventListener('click', () => {
-    menuCategoria.classList.toggle('bx-x');   /** a que e chama , para aparecer o x no menu */
-    navCategoria.classList.toggle('show');    /** chama o menu */
-});
-
